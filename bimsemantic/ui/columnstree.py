967ff9da92241f9cb0121088033c26fa841327ec
Florian Neukirchen <mail@riannek.de>
2024-11-11 11:40:04 +0100

    Add one more level

--- bimsemantic/ui/columnstree.py
+++ bimsemantic/ui/columnstree.py
@@ -6,17 +6,33 @@ from PySide6.QtGui import QAction, QFont, QIcon
 class ColumnsTreeModel(QTreeWidget):
     def __init__(self, data, parent=None):
         super(ColumnsTreeModel, self).__init__(parent)
+        self.first_cols = ["Type", "ID", "Name", "GUID"]
         self.setHeaderHidden(True)
         self.setupModelData(data)
         self.expandAll()
 
     def setupModelData(self, data):
+        infocols_item = QTreeWidgetItem(self)
+        infocols_item.setText(0, "Info Columns")
+        infocols_item.setFlags(infocols_item.flags() | Qt.ItemFlag.ItemIsAutoTristate | Qt.ItemFlag.ItemIsUserCheckable)
+        
+        for col in self.first_cols[1:]:
+            col_item = QTreeWidgetItem(infocols_item)
+            col_item.setText(0, col)
+            col_item.setFlags(col_item.flags() | Qt.ItemFlag.ItemIsUserCheckable)
+            col_item.setCheckState(0, Qt.CheckState.Checked)
+        
+        psets_item = QTreeWidgetItem(self)
+        psets_item.setText(0, "Property Sets")
+        psets_item.setFlags(psets_item.flags() | Qt.ItemFlag.ItemIsAutoTristate | Qt.ItemFlag.ItemIsUserCheckable)
+
         pset_info = data.pset_info
+
         for pset_name, pset_props in pset_info.items():
-            pset_item = QTreeWidgetItem(self)
+            pset_item = QTreeWidgetItem(psets_item)
             pset_item.setText(0, pset_name)
             pset_item.setFlags(pset_item.flags() | Qt.ItemFlag.ItemIsAutoTristate | Qt.ItemFlag.ItemIsUserCheckable)
-            self.addTopLevelItem(pset_item)
+
             for prop in pset_props:
                 prop_item = QTreeWidgetItem(pset_item)
                 prop_item.setText(0, prop)
