6c606107621fec571febb3b83cf13d123fb5cf0f
Florian Neukirchen <mail@riannek.de>
2024-11-12 09:19:00 +0100

    Port IfcTreeItem and LocationTreeModel to use multiple files

--- bimsemantic/util/ifcfile.py
+++ bimsemantic/util/ifcfile.py
@@ -1,5 +1,5 @@
 import ifcopenshell
-from collections import OrderedDict
+# from collections import OrderedDict
 import os
 
 
@@ -30,7 +30,7 @@ class IfcFile():
         return self._pset_info
     
     def _get_pset_info(self):
-        pset_info = OrderedDict()
+        pset_info = {}
         psets = self._model.by_type("IfcPropertySet")
         for pset in psets:
             if not pset.Name in pset_info:
@@ -40,14 +40,81 @@ class IfcFile():
                     pset_info[pset.Name].append(prop.Name)
         return pset_info
     
-    def get_pset_cols(self):
-        cols = []
-        for _, pset_props in self.pset_info.items():
-            for prop in pset_props:
-                cols.append(prop)
-        return cols
+    # def get_pset_cols(self):
+    #     cols = []
+    #     for _, pset_props in self.pset_info.items():
+    #         for prop in pset_props:
+    #             cols.append(prop)
+    #     return cols
     
-    def pset_items(self):
-        for pset_name, pset_props in self.pset_info.items():
-            for prop in pset_props:
-                yield pset_name, prop
+    # def pset_items(self):
+    #     for pset_name, pset_props in self.pset_info.items():
+    #         for prop in pset_props:
+    #             yield pset_name, prop
+
+
+class IfcFiles():
+    def __init__(self):
+        self._ifcfiles = []
+    
+    def add_file(self, filename):
+        ifcfile = IfcFile(filename)
+        self._ifcfiles.append(ifcfile)
+
+        print(f"Added file {ifcfile.filename}")
+
+
+    
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            return self._ifcfiles[index]
+        elif isinstance(index, str):
+            for ifcfile in self._ifcfiles:
+                if ifcfile.filename == index:
+                    return ifcfile
+        raise IndexError(f"Index {index} not found.")
+    
+    def __len__(self):
+        return len(self._ifcfiles)
+    
+    def __iter__(self):
+        for ifcfile in self._ifcfiles:
+            yield ifcfile
+
+    def get_element_by_guid(self, guid):
+        for ifcfile in self._ifcfiles:
+            element = ifcfile.model.by_guid(guid)
+            if element:
+                return element
+        return None
+
+    def get_element(self, filename, id):
+        try:
+            ifc_file = self[filename]
+        except IndexError:
+            None
+        try:
+            element = ifc_file.model.by_id(id)
+        except RuntimeError:
+            return None
+        return element
+
+    def get_project(self):
+        try:
+            project = self._ifcfiles[0].model.by_type("IfcProject")[0]
+        except IndexError:
+            return None
+        return project
+
+
+    @property
+    def pset_info(self):
+        pset_info = {}
+        for ifcfile in self._ifcfiles:
+            for pset_name, pset_props in ifcfile.pset_info.items():
+                if not pset_name in pset_info:
+                    pset_info[pset_name] = []
+                for prop in pset_props:
+                    if not prop in pset_info[pset_name]:
+                        pset_info[pset_name].append(prop)
+        return pset_info
