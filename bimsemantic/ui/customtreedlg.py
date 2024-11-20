b0be054c119fda91bd8c059524e01ced6089f522
Florian Neukirchen <mail@riannek.de>
2024-11-20 13:26:06 +0100

    PropertyListItem

--- bimsemantic/ui/customtreedlg.py
+++ bimsemantic/ui/customtreedlg.py
@@ -9,11 +9,18 @@ from PySide6.QtWidgets import (
     QLineEdit,
     QListWidget,
     QListWidgetItem,
+    QTreeWidget,
+    QTreeWidgetItem,
     QPushButton,
     QStyle,
 )
 
 
+
+class PropertyListItem(QListWidgetItem):
+    def __init__(self, text, pset_name):
+        super().__init__(text)
+        self.pset_name = pset_name
 
 
 class CustomTreeDialog(QDialog):
@@ -32,7 +39,7 @@ class CustomTreeDialog(QDialog):
         name_layout.addWidget(self.name)
         layout.addLayout(name_layout)
 
-        self.left_list = QListWidget()
+        self.left_list = QTreeWidget()
         self.right_list = QListWidget()
 
         self.left_list.setMinimumWidth(200)
@@ -40,10 +47,15 @@ class CustomTreeDialog(QDialog):
         self.right_list.setMinimumWidth(200)
         self.left_list.setMinimumHeight(300)
 
-        for i in range(10):
-            item = QListWidgetItem(f"Element {i}")
-            self.left_list.addItem(item)
+        for i in range(3):
+            pset_item = QTreeWidgetItem(self.left_list)
+            pset_item.setText(0, f"Property Set {i}")
+            for j in range(5):
+                prop_item = QTreeWidgetItem(pset_item)
+                prop_item.setText(0, f"Property {i}.{j}")
 
+        self.left_list.expandAll()
+        self.left_list.setHeaderHidden(True)
 
         list_layout = QHBoxLayout()
         left_button_layout = QVBoxLayout()
@@ -97,14 +109,22 @@ class CustomTreeDialog(QDialog):
     def add_item(self):
         selected_items = self.left_list.selectedItems()
         for item in selected_items:
-            self.left_list.takeItem(self.left_list.row(item))
-            self.right_list.addItem(item)
+            if item.parent() is not None:  
+                prop_item = PropertyListItem(item.text(0), item.parent().text(0))
+                self.right_list.addItem(prop_item)
+                item.parent().removeChild(item)
 
     def remove_item(self):
         selected_items = self.right_list.selectedItems()
         for item in selected_items:
             self.right_list.takeItem(self.right_list.row(item))
-            self.left_list.addItem(item)
+            pset_text = item.pset_name
+            for i in range(self.left_list.topLevelItemCount()):
+                pset_item = self.left_list.topLevelItem(i)
+                if pset_item.text(0) == pset_text:
+                    prop_item = QTreeWidgetItem(pset_item)
+                    prop_item.setText(0, item.text())
+                    pset_item.sortChildren(0, Qt.AscendingOrder)
 
     def move_item_up(self):
         current_row = self.right_list.currentRow()
@@ -127,7 +147,8 @@ class CustomTreeDialog(QDialog):
     def get_items(self):
         items = []
         for i in range(self.right_list.count()):
-            items.append(self.right_list.item(i).text())
+            item = self.right_list.item(i)
+            items.append((item.pset_name, item.text()))
         return items
 
 if __name__ == "__main__":
