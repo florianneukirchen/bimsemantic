1e57d1665b510abdb80df4998d0228187f46e3a3
Florian Neukirchen <mail@riannek.de>
2024-11-23 16:50:20 +0100

    DetailsBaseclass

--- bimsemantic/ui/detailview.py
+++ bimsemantic/ui/detailview.py
@@ -1,117 +1,116 @@
 import ifcopenshell.util.element
 from .treebase import TreeItem, TreeModelBaseclass
 
+class DetailsBaseclass(TreeModelBaseclass):
+    def owner_history_item(self, owner_history, parent):
+        """Create a tree item for the owner history
+        
+        Including sub-items for the owning person, organization and application.
+        Also adds the item to parent.
 
-def owner_history_item(owner_history, parent):
-    """Create a tree item for the owner history
-    
-    Including sub-items for the owning person, organization and application.
-    Also adds the item to parent.
+        :param owner_history: The IfcOwnerHistory entity
+        :type owner_history: ifcopenshell entity
+        :param parent: The parent tree item
+        :type parent: TreeItem
+        :return: The tree item
+        :rtype: TreeItem
+        """
+        history_item = TreeItem([f"Owner History (ID {owner_history.id()})"], parent=parent)
+        parent.appendChild(history_item)
 
-    :param owner_history: The IfcOwnerHistory entity
-    :type owner_history: ifcopenshell entity
-    :param parent: The parent tree item
-    :type parent: TreeItem
-    :return: The tree item
-    :rtype: TreeItem
-    """
-    history_item = TreeItem([f"Owner History (ID {owner_history.id()})"], parent=parent)
-    parent.appendChild(history_item)
+        owning_user_item = TreeItem(["Owning User"], parent=history_item)
+        history_item.appendChild(owning_user_item)
 
-    owning_user_item = TreeItem(["Owning User"], parent=history_item)
-    history_item.appendChild(owning_user_item)
+        person = owner_history.OwningUser.ThePerson
+        person_item = TreeItem([f"Person {person.GivenName}"], parent=owning_user_item)
+        owning_user_item.appendChild(person_item)
 
-    person = owner_history.OwningUser.ThePerson
-    person_item = TreeItem([f"Person {person.GivenName}"], parent=owning_user_item)
-    owning_user_item.appendChild(person_item)
+        for k,v in person.get_info().items():
+            if v and not k in ["id", "type"]:
+                person_item.appendChild(
+                    TreeItem([k,v], parent=person_item)
+                )
 
-    for k,v in person.get_info().items():
-        if v and not k in ["id", "type"]:
-            person_item.appendChild(
-                TreeItem([k,v], parent=person_item)
-            )
+        org = owner_history.OwningUser.TheOrganization
+        self.item_with_subitems(org, owning_user_item, f"Organization {org.Name}")
 
-    org = owner_history.OwningUser.TheOrganization
-    print(org)
-    item_with_subitems(org, owning_user_item, f"Organization {org.Name}")
+        owning_app = owner_history.OwningApplication
 
-    owning_app = owner_history.OwningApplication
+        owning_app_item = TreeItem([f"Owning Application {owning_app.ApplicationFullName}"], parent=history_item)
+        history_item.appendChild(owning_app_item)
 
-    owning_app_item = TreeItem([f"Owning Application {owning_app.ApplicationFullName}"], parent=history_item)
-    history_item.appendChild(owning_app_item)
+        for k,v in owning_app.get_info().items():
+            try:
+                is_org = v.is_a("IfcOrganization")
+            except AttributeError:
+                is_org = False
+            if is_org: 
+                self.item_with_subitems(v, owning_app_item, f"Organization {v.Name}")
+            elif k not in ["id", "type"]:
+                owning_app_item.appendChild(
+                    TreeItem([k,v], parent=owning_app_item)
+                )
 
-    for k,v in owning_app.get_info().items():
-        try:
-            is_org = v.is_a("IfcOrganization")
-        except AttributeError:
-            is_org = False
-        if is_org: 
-            item_with_subitems(v, owning_app_item, f"Organization {v.Name}")
-        elif k not in ["id", "type"]:
-            owning_app_item.appendChild(
-                TreeItem([k,v], parent=owning_app_item)
-            )
+        for k,v in owner_history.get_info().items():
+            if v and not k in ["id", "type", "OwningUser", "OwningApplication"]:
+                history_item.appendChild(
+                    TreeItem([k,v], parent=history_item)
+                )
 
-    for k,v in owner_history.get_info().items():
-        if v and not k in ["id", "type", "OwningUser", "OwningApplication"]:
-            history_item.appendChild(
-                TreeItem([k,v], parent=history_item)
-            )
+        return history_item    
 
-    return history_item    
+    def item_with_subitems(self, entity, parent, label):
+        """Create a tree item with several key-value pair subitems
 
-def item_with_subitems(entity, parent, label):
-    """Create a tree item with several key-value pair subitems
+        Make subitems for the key value pairs in the dict returned by 
+        entity.get_info().
+        
+        :param entity: The IfcOpenShell entity
+        :type org: ifcopenshell entity
+        :param parent: The parent tree item
+        :type parent: TreeItem
+        :param label: The label for the main item
+        :type label: str
+        :return: The tree item
+        :rtype: TreeItem
+        """
+        main_item = TreeItem([label], parent=parent)
+        parent.appendChild(main_item)
 
-    Make subitems for the key value pairs in the dict returned by 
-    entity.get_info().
-    
-    :param entity: The IfcOpenShell entity
-    :type org: ifcopenshell entity
-    :param parent: The parent tree item
-    :type parent: TreeItem
-    :param label: The label for the main item
-    :type label: str
-    :return: The tree item
-    :rtype: TreeItem
-    """
-    main_item = TreeItem([label], parent=parent)
-    parent.appendChild(main_item)
+        for k,v in entity.get_info().items():
+            if v and not k in ["id", "type"]:
+                main_item.appendChild(
+                    TreeItem([k,v], parent=main_item)
+                )
 
-    for k,v in entity.get_info().items():
-        if v and not k in ["id", "type"]:
-            main_item.appendChild(
-                TreeItem([k,v], parent=main_item)
-            )
+        return main_item
 
-    return main_item
-
-def address_item(address, parent):
-    """Create a tree item for an address
-    
-    :param address: The IfcPostalAddress entity
-    :type address: ifcopenshell entity
-    :param parent: The parent tree item
-    :type parent: TreeItem
-    :return: The tree item
-    :rtype: TreeItem
-    """
-    add_item = TreeItem(["Address"], parent=parent)
-    parent.appendChild(add_item)
-    address_lines = address.AddressLines
-    for i, line in enumerate(address_lines):
-        add_item.appendChild(
-            TreeItem([f"Address line {i+1}", line], parent=add_item)
-        )
-    for k,v in address.get_info().items():
-        if v and k not in ["id", "type", "AddressLines"]:
+    def address_item(self, address, parent):
+        """Create a tree item for an address
+        
+        :param address: The IfcPostalAddress entity
+        :type address: ifcopenshell entity
+        :param parent: The parent tree item
+        :type parent: TreeItem
+        :return: The tree item
+        :rtype: TreeItem
+        """
+        add_item = TreeItem(["Address"], parent=parent)
+        parent.appendChild(add_item)
+        address_lines = address.AddressLines
+        for i, line in enumerate(address_lines):
             add_item.appendChild(
-                TreeItem([k,v], parent=add_item)
-        )
-            
-    return add_item
+                TreeItem([f"Address line {i+1}", line], parent=add_item)
+            )
+        for k,v in address.get_info().items():
+            if v and k not in ["id", "type", "AddressLines"]:
+                add_item.appendChild(
+                    TreeItem([k,v], parent=add_item)
+            )
+                
+        return add_item
 
-class IfcDetailsTreeModel(TreeModelBaseclass):
+class IfcDetailsTreeModel(DetailsBaseclass):
     """Model for the tree view of the details dock widget
     
     :param id: The ID of the element in the first file of filenames
@@ -133,11 +132,11 @@ class IfcDetailsTreeModel(TreeModelBaseclass):
         """
         if isinstance(value, ifcopenshell.entity_instance):
             if value.is_a("IfcOwnerHistory"):
-                return owner_history_item(value, parent)
+                return self.owner_history_item(value, parent)
             elif value.is_a("IfcOrganization"):
-                return item_with_subitems(value, parent, "Organization {value.Name}")
+                return self.item_with_subitems(value, parent, "Organization {value.Name}")
             elif value.is_a("IfcPostalAddress"):
-                return address_item(value, parent)
+                return self.address_item(value, parent)
 
             try:
                 name = value.Name
@@ -160,11 +159,11 @@ class IfcDetailsTreeModel(TreeModelBaseclass):
         self.rows_spanned = []
 
         object_item = parent
-        object = data
+        ifc_object = data
 
-        info = object.get_info()
+        info = ifc_object.get_info()
         object_item.appendChild(
-            TreeItem(["Name", object.Name], parent=object_item)
+            TreeItem(["Name", ifc_object.Name], parent=object_item)
         )
         object_item.appendChild(
             TreeItem(["Type", info["type"]], parent=object_item)
@@ -174,16 +173,16 @@ class IfcDetailsTreeModel(TreeModelBaseclass):
                 TreeItem(["Object Type", info["ObjectType"]], parent=object_item)
             )
         object_item.appendChild(
-            TreeItem(["IFC ID", object.id()], parent=object_item)
+            TreeItem(["IFC ID", ifc_object.id()], parent=object_item)
         )
 
         # Check if the ID is alway the same
         if len(self.filenames) > 1:
             ids = []
             for filename in self.filenames[1:]:
-                inotherfile = self._mainwindow.ifcfiles.get_element_by_guid(object.GlobalId, filename)
+                inotherfile = self._mainwindow.ifcfiles.get_element_by_guid(ifc_object.GlobalId, filename)
                 ids.append(inotherfile.id())
-            if not all([id == object.id() for id in ids]):
+            if not all([id == ifc_object.id() for id in ids]):
                 for i, id in enumerate(ids):
                     object_item.appendChild(
                         TreeItem([f"IFC ID ({i+1})", id], parent=object_item)
@@ -191,7 +190,7 @@ class IfcDetailsTreeModel(TreeModelBaseclass):
         
 
         object_item.appendChild(
-            TreeItem(["Global ID", object.GlobalId], parent=object_item)
+            TreeItem(["Global ID", ifc_object.GlobalId], parent=object_item)
         )
 
 
@@ -203,7 +202,7 @@ class IfcDetailsTreeModel(TreeModelBaseclass):
                 self.new_item(k, v, info_item)
                 
 
-        owner_history_item(object.OwnerHistory, info_item)
+        self.owner_history_item(ifc_object.OwnerHistory, info_item)
 
         # Filenames
         info_item.appendChild(
@@ -220,28 +219,28 @@ class IfcDetailsTreeModel(TreeModelBaseclass):
                 )
 
         # Spatial relations
-        if object.is_a("IfcElement"):
+        if ifc_object.is_a("IfcElement"):
             try:
-                self.new_item("Contained in", object.ContainedInStructure[0].RelatingStructure, info_item)
+                self.new_item("Contained in", ifc_object.ContainedInStructure[0].RelatingStructure, info_item)
             except IndexError:
                 pass
 
-        elif object.is_a("IfcSpatialStructureElement"):
-            decomposes = object.Decomposes
+        elif ifc_object.is_a("IfcSpatialStructureElement"):
+            decomposes = ifc_object.Decomposes
             if decomposes:
                 self.new_item("Contained in", decomposes[0].RelatingObject, info_item)
 
             contains_item = TreeItem(["Contains"], parent=info_item)
             info_item.appendChild(contains_item)
             try:
-                elements = object.ContainsElements[0].RelatedElements
+                elements = ifc_object.ContainsElements[0].RelatedElements
             except IndexError:
                 elements = []
             for element in elements:
                 contains_item.appendChild(
                     TreeItem([element.is_a(), element.id(), element.Name], parent=contains_item)
                 )
-            iscomposedby = object.IsDecomposedBy
+            iscomposedby = ifc_object.IsDecomposedBy
             if iscomposedby:
                 for obj in list(iscomposedby[0].RelatedObjects):
                     contains_item.appendChild(
@@ -249,7 +248,7 @@ class IfcDetailsTreeModel(TreeModelBaseclass):
                     )
 
         # Property Sets
-        psets = ifcopenshell.util.element.get_psets(object)
+        psets = ifcopenshell.util.element.get_psets(ifc_object)
         for pset_name, pset in psets.items():
             pset_item = TreeItem([pset_name], parent=object_item)
             self.rows_spanned.append(pset_item)
@@ -262,8 +261,8 @@ class IfcDetailsTreeModel(TreeModelBaseclass):
 
         # Materials
         materials = []
-        if object.HasAssociations:
-            for association in object.HasAssociations:
+        if ifc_object.HasAssociations:
+            for association in ifc_object.HasAssociations:
                 if association.is_a("IfcRelAssociatesMaterial"):
                     materials.append(association.RelatingMaterial)
     
@@ -289,7 +288,7 @@ class IfcDetailsTreeModel(TreeModelBaseclass):
 
 
 
-class OverviewTreeModel(TreeModelBaseclass):
+class OverviewTreeModel(DetailsBaseclass):
     """Overview model for the details dock widget showing info about the files
     
     :param parent: The parent widget (main window)
@@ -363,7 +362,7 @@ class OverviewTreeModel(TreeModelBaseclass):
                     )
                 address = building.BuildingAddress
                 if address:
-                    address_item(address, building_item)
+                    self.address_item(address, building_item)
 
             try:
                 crs = ifcfile.model.by_type("IfcCoordinateReferenceSystem")[0]
