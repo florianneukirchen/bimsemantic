5b180bbbbccea068ee4b2bac39e6796f04c8316f
Florian Neukirchen <mail@riannek.de>
2024-12-09 16:18:18 +0100

    IdsValidator

---
+++ bimsemantic/util/validation.py
@@ -0,0 +1,34 @@
+import ifctester
+import ifctester.reporter
+import ifcopenshell
+import os
+
+class IfsValidator:
+    def __init__(self, filename):
+        if not os.path.exists(filename):
+            raise FileNotFoundError(f"File {filename} not found.")
+        self.filename = filename
+        self.rules = ifctester.ids.open(filename)
+        self.report = None
+
+    def validate(self, model):
+        self.rules.validate(model)
+        self.report = ifctester.reporter.Bcf(self.rules)
+        # Return the report as JSON
+        return self.report.report()
+    
+    def save_bcf(self, filename):
+        if not self.report:
+            raise ValueError("No report. Run validate() first.")
+        self.report.to_file(filename)
+
+
+if __name__ == "__main__":
+    folder = '/media/riannek/PortableSSD/ids_testcases'
+    ifc_file = os.path.join(folder, "IDS_wooden-windows_IFC.ifc")
+    model = ifcopenshell.open(ifc_file)
+    ids_file = os.path.join(folder, "IDS_wooden-windows.ids")
+    validator = IfsValidator(ids_file)
+    report = validator.validate(model)
+    print(report)
+    validator.save_bcf("report.bcfzip")
